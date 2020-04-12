32bcdfd03f1842593e00ffe3af687e95698b20a5
Drashtant Nayak <drashtantnayak@MacBook-Air.local>
2020-04-12 16:22:39 -0300

    Final touch for email function

--- mailer.py
+++ mailer.py
@@ -5,22 +5,21 @@ import smtplib
 from email.mime.text      import MIMEText
 from email.header         import Header
 
-def sendemail(subject,body):
+def sendemail(subject,body,recipients):
     # MIME definition
     msg = MIMEText(body, 'plain', 'utf-8')
     # Setting Subject with proper utf format	
     msg['Subject'] = Header(subject, 'utf-8')
     # Setting up from emailer
-    msg['From'] = "Dev-INWK admin@devinwk.com"
+    msg['From'] = "no-reply@xyz.com"
     # Receivers email address
-    recipients = "drashtant391993@gmail.com"
     msg['To'] = recipients
     # smtp with proper format of port and timeout 
     s = smtplib.SMTP('smtp.gmail.com', 587, timeout=10)
     s.set_debuglevel(1)
     try:
         s.starttls()
-        s.login("dynotecxmailer@gmail.com", "DynSol1!")
+        s.login("gmail_username", "gmail_password")
         s.sendmail(msg['From'], recipients, msg.as_string())
     finally:
         s.quit()
